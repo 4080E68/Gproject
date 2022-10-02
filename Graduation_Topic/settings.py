3b2a33aaaaa6fe2b184cd97f09ab38675e8db125
林昱鋐 <zxcv96197@gmail.com>
2022-10-02 11:54:36 +0800

    1002

--- Graduation_Topic/settings.py
+++ Graduation_Topic/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # 本機測試
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 上傳
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # 本機測試
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 上傳
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
